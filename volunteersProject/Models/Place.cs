0714dbbee37f6815c841b9ca496973ec9ccdfcf9
Емельдяев Антон <aemelia2004@gmail.com>
2026-04-07 13:58:40 +0500

    Подключил БД

---
+++ volunteersProject/Models/Place.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace volunteersProject.Models;
+
+public partial class Place
+{
+    public int Id { get; set; }
+
+    public string PlaceName { get; set; } = null!;
+
+    public virtual ICollection<Doing> Doings { get; set; } = new List<Doing>();
+}
---
+++ volunteersProject/Models/StatusesEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace volunteersProject.Models;
+
+public partial class StatusesEvent
+{
+    public int Id { get; set; }
+
+    public string StatusName { get; set; } = null!;
+
+    public virtual ICollection<Doing> Doings { get; set; } = new List<Doing>();
+}
